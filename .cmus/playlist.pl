63fa65ae7b0340124ec89755f567acfafd4aee89
Kevin Most <kevin@kevinmost.com>
2014-12-11 17:04:46 -0500

    Added shorthand aliases for Perka stuff, added a fresh-install script for Macbooks to install a bunch of packages and apps from brew

--- .cmus/playlist.pl
+++ .cmus/playlist.pl
@@ -1,1 +1,2 @@
 /Users/kevinmost/Dropbox/music/Opeth/Orchid/Opeth - Forest of October.mp3
+/Users/kevinmost/Dropbox/music/Amon Amarth/[2013] Deceiver Of The Gods (Limited Edition)/[CD 1] Deceiver Of The Gods/01 - Deceiver Of The Gods.mp3
