583d63cba08c6ff4ace3eebb5ffe505bd238f6b9
Kevin Most <kevin@kevinmost.com>
2015-11-02 18:58:38 -0500

    New branch for OS X based off master

---
+++ fresh-install-mac.sh
@@ -0,0 +1,79 @@
+# This will be faster if we install our ethernet drivers first
+echo "This will be faster if you download your ethernet drivers first"
+open http://www.asix.com.tw/download.php?sub=driverdetail&PItemID=131
+read -p "Press [Enter] if you have already installed those drivers and restarted your computer"
+
+# This command is the worst because it requires GUI interactions
+clear
+echo "Time to install Xcode dev tools, click Install on the popup. NOT Get XCode!!!"
+xcode-select --install
+
+# Install homebrew
+clear
+echo "Installing Homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Tap brew taps
+clear
+echo "Tapping extra Homebrew Casks"
+brew tap caskroom/cask
+brew tap caskroom/versions
+brew tap homebrew/science
+brew tap homebrew/versions
+brew tap jlhonora/lsusb
+brew tap mopidy/mopidy
+
+# Pre-installation reqs.
+clear
+echo "Installing some dependencies before we install all the big packages, this might hang for a while, but it's okay!"
+brew install Caskroom/cask/xquartz
+brew install brew-cask
+brew cask install java
+
+# Install brew packages
+clear
+echo "Installing command-line tools via brew"
+brew install ack android-sdk apache-spark bash brew-cask cassandra cloc cmus coreutils cowsay cuetools cvs daemonize dotwrp fortune gawk gcc git gnu-sed go graphicsmagick heroku-toolbelt hub id3lib imagemagick jetty jq libev libfreenect lsusb maven mercurial mobile-shell mopidy mpd mutt ncmpcpp node nvm p7zip pandoc parallel python s3cmd scala solr the_silver_searcher tree v8 watch wget wine winetricks zenity zsh
+
+# Post-command-line-setup stuff
+# mpd starts at boot
+mkdir -p ~/Library/LaunchAgents
+ln -sfv /usr/local/opt/mpd/*.plist ~/Library/LaunchAgents
+# Why does nvm want me to make this directory? I don't even know. Thanks JS devs.
+mkdir ~/.nvm
+
+# Install brew-cask packages
+clear
+echo "Installing GUI apps via brew-cask"
+brew cask install adium alfred android-studio asepsis basictex bettertouchtool betterzipql clipmenu deluge disk-inventory-x dropbox filezilla flux genymotion gimp google-chrome intellij-idea iterm2-nightly karabiner keepingyouawake keka mpv qlcolorcode qlmarkdown qlprettypatch qlstephen quicklook-csv quicklook-json seil spark spectacle steam sublime-text3 virtualbox webpquicklook xquartz
+
+clear
+echo "Fixing the awful things that OS X thinks are okay to set as defaults"
+# Enable press-and-hold repeating characters
+defaults write -g ApplePressAndHoldEnabled -bool false
+# Seriously, no hidden files or extensions by default? What is this, Windows?
+defaults write com.apple.finder AppleShowAllFiles TRUE;killall Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Speed up animations
+defaults write com.apple.dock expose-animation-duration -float 0.15;killall Dock
+# Proper anti-aliasing
+defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
+
+clear
+echo "Installing the best shell"
+# Add zsh to /etc/shells
+sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
+# Change to zsh
+chsh -s /usr/local/bin/zsh
+# Install oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
+# Install antigen and syntax highlighter
+curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh
+source antigen.zsh
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Install Android ADB + fastboot. Because android > xcode, there is a no-ui option, too!
+android update sdk --no-ui --filter 'platform-tools'
+
+# Restore muh freedoms
+python bin/fix-macosx.py
---
+++ scripts-osx/cmus-macbook-mediakeys.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import subprocess
+
+# PyObjC-related imports
+from AppKit import NSApplication, NSSystemDefined, NSApplicationActivationPolicyProhibited
+from PyObjCTools import AppHelper
+
+
+KEY_UP = 11
+
+
+class KeySocketApp(NSApplication):
+
+
+    def sendEvent_(self, event):
+        if event.type() is NSSystemDefined and event.subtype() is 8:
+            data = event.data1()
+            keyCode = (data & 0xFFFF0000) >> 16
+            keyFlags = (data & 0x0000FFFF)
+            keyState = (keyFlags & 0xFF00) >> 8
+
+            if keyState is KEY_UP:
+                if (keyCode == 20) or (keyCode == 18):
+                    subprocess.call(['cmus-remote', '-r'])
+                elif keyCode == 16: 
+                    subprocess.call(['cmus-remote', '-u'])
+                elif (keyCode == 19) or (keyCode == 17):
+                    subprocess.call(['cmus-remote', '-n'])
+                print keyCode 
+
+if __name__ == '__main__':
+    app = KeySocketApp.sharedApplication()
+    app.setActivationPolicy_(NSApplicationActivationPolicyProhibited)
+    AppHelper.runEventLoop()
